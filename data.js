c3a3d9c43ee63d7f18f4a87532837da174b5affa
Segolsysit <senthil.segolsys@gmail.com>
2023-08-08 13:45:30 +0530

    module change

---
+++ data.js
@@ -0,0 +1,7 @@
+const dataArray = [
+    { id: 1, name: "Object 1" },
+    { id: 2, name: "Object 2" },
+
+];
+
+export default dataArray;
---
+++ log.js
@@ -0,0 +1,3 @@
+import dataArray from "./data.js"; // Adjust the path if necessary
+
+console.log("Data from dataArray:", dataArray);
